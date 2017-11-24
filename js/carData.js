bab399be5fd8aead229b4fdd4ce49df3ad0c887a
Wisniewski <s_wisniewski@fanshawec.ca>
2017-11-24 11:33:56 -0500

    New stuff

---
+++ js/carData.js
@@ -0,0 +1,12 @@
+var carData = {
+
+  F55: {
+
+    headline: "Mini Cooper F55",
+
+    text: "It’s the turbocharged motoring icon, with more handles to love. The MINI 5 door boasts the same iconic good looks as its 3 door brother, but features an extra pair of passenger doors for practical access to its spacious cabin. And despite its slightly longer frame, the MINI 5 door’s wide, athletic stance and wheels-pushed-out design deliver the corner-carving agility you’d expect from any MINI."
+
+  }
+
+}
+}
---
+++ js/main.js
@@ -0,0 +1,24 @@
+(function () {
+  // start with retrieving the elements from the page, and then adding event handling. then write the logic. refer to the seasons example / homework
+  var theImages = document.querySelectorAll ('.data-reff'),
+  var theHeading = document.querySelector('.modelName'),
+  var thePrice = document.querySelector('.priceInfo p'),
+  var theDetails = document.querySelector('.modelDetails p'),
+  appliedClass;
+
+  function changeElements() {
+    debugger;
+    let objectIndex = carData[this.id];
+
+    theHeading.firstChild.nodeValue = carData.headline;
+    theDetails.firstChild.nodevale = carData.text;
+    appliedClass = this.id;
+  }
+
+  theImages.forEach(function(element, index) {
+
+    element.addEventListener('click', changeElements, false);
+
+    });
+
+})();
